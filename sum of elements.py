2eedba83eb49334b0ba8664af368990e32fa95e3
sushma8919 <162419003+sushma8919@users.noreply.github.com>
2025-03-28 15:46:38 +0530

    Add files via upload

---
+++ sum of elements.py
@@ -0,0 +1,6 @@
+mylist=input("enter a list of numbers seperated by space:")
+mylist=list(map(int,mylist.split()))
+sum=0
+for num in mylist:
+    sum+=num
+    print("the sum of the numbers is:",sum)
