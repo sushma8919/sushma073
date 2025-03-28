712fc264c2f218f963a3af0da4acb669121353d4
sushma8919 <162419003+sushma8919@users.noreply.github.com>
2025-03-28 15:45:48 +0530

    Add files via upload

---
+++ between 1 and 100.py
@@ -0,0 +1,5 @@
+num=int(input("enter any number:"))
+if num>=1 and num<=100:
+      print("the given number",num,"is in between 1 and 100")
+else:
+      print("the given number",num,"is not between 1 and 100")
