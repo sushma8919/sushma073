eddf06a407a5fc0ca5d6bddf6c4658e7eecc2f03
sushma8919 <162419003+sushma8919@users.noreply.github.com>
2025-06-10 11:52:54 +0530

    Add files via upload

---
+++ processor.py
@@ -0,0 +1,6 @@
+import platform
+system_name=platform.system()
+node_name=platform.node()
+processor_name=platform.processor()
+print("system name:",system_name)
+print("processor name:",processor_name)
