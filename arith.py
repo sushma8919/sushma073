e69a5d508e7607dd1ab18d482cc5200c74279a34
sushma8919 <162419003+sushma8919@users.noreply.github.com>
2025-06-10 11:48:42 +0530

    Add files via upload

---
+++ arith.py
@@ -0,0 +1,14 @@
+def add(x,y):
+    return x+y
+def subtract(x,y):
+    return x-y
+def multiply(x,y):
+    return x*y
+def divide(x,y):
+    return x/y
+num1=float(input("enter the first number:"))
+num2=float(input("enter the second number:"))
+print("addition:",add(num1,num2))
+print("subtraction:",subtract(num1,num2))
+print("multiplication:",multiply(num1,num2))
+print("division:",divide(num1,num2))
