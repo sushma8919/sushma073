86d79b39a4a23604bd989d5f696055a0735513d4
sushma8919 <162419003+sushma8919@users.noreply.github.com>
2025-06-10 11:55:09 +0530

    Add files via upload

---
+++ radius.py
@@ -0,0 +1,10 @@
+class circle:
+    def __init__(self,radius):
+        self.radius=radius
+    def area(self):
+        return 3.14*self.radius*self.radius
+def get_circle(r):
+        return circle(r)
+c=get_circle(5)
+print("area of circle:",c.area())
+    
